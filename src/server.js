dc6fef460dd895ae37a74c19ddf760f1257c1f5f
karen <karenkaka2010@hotmail.com>
2021-11-08 22:26:43 -0300

    build: add cors

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ require('./database');
 
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333); 
+app.listen(process.env.PORT || 3333); 
