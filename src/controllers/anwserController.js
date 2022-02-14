0319e58866ea29c46b610688b4a1079f146e94de
karen <karenkaka2010@hotmail.com>
2022-02-14 20:25:56 -0300

    fix: answer controoler

--- src/controllers/anwserController.js
+++ src/controllers/anwserController.js
@@ -10,13 +10,14 @@ module.exports = {
     const { message_id } = req.params;
     try {
       if (!message_id) return res.status(400).json({ msg: 'missing the required message id' });
-      const messages = await Messages.findByPk(messages_id);
+      const messages = await Messages.findByPk(message_id);
       if (!messages) return res.status(400).json({ msg: 'messages not found' });
       if (!answer) return res.status(400).json({ msg: 'missing the field answer' });
 
       const answerResponse = await Answer.create({ user_id, message_id, answer });
       return res.status(200).json(answerResponse);
     } catch (error) {
+      console.log({ error });
       return res.status(500).json({ msg: 'Internal server error' });
     }
   },
